0258319469df174460e04e99fabddc9f225c3c32
TxusBlasco <jesusgarciablasco@gmail.com>
2024-01-18 07:50:10 +0100

    Refactor the endpoint /v1/instruments/fetch to return a 201 answer

--- core/infrastructure/controllers/extractor.py
+++ core/infrastructure/controllers/extractor.py
@@ -1,16 +1,21 @@
-from fastapi import APIRouter, status, Body
+from fastapi import APIRouter, status, Body, BackgroundTasks
 from core.application.services.extractor import ExtractorAppService
 from core.domain.models.extractor import InstrumentList
+from fastapi.responses import JSONResponse
 
 extraction = APIRouter()
 
 
 @extraction.post("/v1/instruments/fetch",
                  status_code=status.HTTP_201_CREATED,
-                 tags=["instrument_extraction"])
-async def start_extraction(instruments: InstrumentList = Body(...)):
+                 tags=["Start a stream of candles, being sent to kafka"])
+async def start_candle_extraction(background_tasks: BackgroundTasks, instruments: InstrumentList = Body(...)):
     """
     Endpoint to start the stream of price extractions. Needs a body with a list of candles to fetch
     :return: None
     """
-    ExtractorAppService(insts=instruments.instruments).fetch_stream()
+    start = ExtractorAppService(insts=instruments.instruments)
+    background_tasks.add_task(start.fetch_stream)
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content={"message": "Candle extraction stream successfully started"})
